0e58e46e78949a11e99e4c05349d3e6c9b9adeee
furkanozanyuce <furkanozanyuce@gmail.com>
2024-11-06 00:18:51 +0300

    adjusted note css to make it the same as the first iteration

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -14,28 +14,28 @@ function Note({ quantity, totalPrice, onIncrement, onDecrement }) {
           <textarea name="note" id='note' placeholder='Siparişinize eklemek istediğiniz bir not var mı?' />
         </label>
 
-        <div className="order-container">
-        {/* Quantity Selector */}
-        <div className="quantity-selector">
-          <button className="decrement" onClick={onDecrement}>-</button>
-          <span className="quantity">{quantity}</span>
-          <button className="increment" onClick={onIncrement}>+</button>
-        </div>
-
-        {/* Order Summary */}
-        <div className="order-summary">
-          <h3>Sipariş Toplamı</h3>
-          <div className="summary-item">
-            <span>Seçimler</span>
-            <span>{(totalPrice - (quantity * 85.50)).toFixed(2)}₺</span>
+        <div className='bottom-side'>
+          <div className="quantity-selector">
+            <button className="decrement btn" onClick={onDecrement}>-</button>
+            <span className="quantity">{quantity}</span>
+            <button className="increment btn" onClick={onIncrement}>+</button>
           </div>
-          <div className="summary-item total">
-            <span>Toplam</span>
-            <span>{totalPrice.toFixed(2)}₺</span>
+          
+          <div className='bottom-most'>
+          <div className="order-summary">
+            <h3>Sipariş Toplamı</h3>
+            <div className="summary-item">
+              <span>Seçimler</span>
+              <span>{(totalPrice - (quantity * 85.50)).toFixed(2)}₺</span>
+            </div>
+            <div className="summary-item total">
+              <span>Toplam</span>
+              <span>{totalPrice.toFixed(2)}₺</span>
+            </div>
           </div>
-          <button className="order-button">SİPARİŞ VER</button>
+          <button className="order-button btn">SİPARİŞ VER</button>
+          </div>
         </div>
-      </div>
       </>
     )
   }
