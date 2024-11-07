f709a4e03f9c5c6472d71bc5d3020856ef6a3813
furkanozanyuce <furkanozanyuce@gmail.com>
2024-11-07 22:32:58 +0300

    created orderdata in mainfrom to use as prop to lift to  successpage, used orderdata to show the order details in the successpage

--- src/MainForm.jsx
+++ src/MainForm.jsx
@@ -14,62 +14,7 @@ import {
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import Header from './components/Header';
-import Size from './components/Size';
 
-
-const extras = [{
-  value: "pepperoni",
-  label: "Pepperoni"
-}, {
-  value: "sosis",
-  label: "Sosis"
-}, {
-  value: "kanadaJambonu",
-  label: "Kanada Jambonu"
-}, {
-  value: "tavukIzgara",
-  label: "Tavuk Izgara"
-}, {
-  value: "sogan",
-  label: "Soğan"
-}, {
-  value: "domates",
-  label: "Domates"
-}, {
-  value: "misir",
-  label: "Mısır"
-}, {
-  value: "sucuk",
-  label: "Sucuk"
-}, {
-  value: "jalepeno",
-  label: "Jalepeno"
-}, {
-  value: "sarimsak",
-  label: "Sarımsak"
-}, {
-  value: "biber",
-  label: "Biber"
-}, {
-  value: "zeytin",
-  label: "Zeytin"
-}, {
-  value: "ananas",
-  label: "Ananas"
-}, {
-  value: "kabak",
-  label: "Kabak"
-}, {
-  value: "peynir",
-  label: "Peynir"
-}
-];
-
-const initalHamur = [
-    {name: "İnce"},
-    {name: "Normal"},
-    {name: "Kalın"}
-]
 
 const initialData = {
   malzemeler: [],
@@ -85,7 +30,7 @@ const errorMessages = {
     warining: "En az 4, en fazla 10 adet ekstra malzeme seçebilirsiniz!"
   };
 
-function MainForm() {
+function MainForm({setOrderData, extras, initalHamur}) {
 
   const [formData, setFormData] = useState(initialData);
   const [isValid, setIsValid] = useState(false);
@@ -165,87 +110,106 @@ function MainForm() {
     
     axios.post("https://reqres.in/api/pizza", formData)
           .then((response) => {
-            console.log(response.data)
+            setOrderData(response.data);
+            history.push("/successpage");
           })
           .catch((error) => {
             console.log(error);
           })
-    history.push("/successpage")
   };
 
   return (
     <>
     <Header />
     <Form onSubmit={handleSubmit}>
-        <FormGroup>
-            <Size onChange={handleChange} value={initalHamur.name}/>
-        </FormGroup>
-        <div className='extra-label'>
-        <Label><h2>Ek Malzemeler</h2>
-                <p className='p-long'>Ek malzemelerinizi seçiniz. 5₺</p>
-                </Label>
+      <FormGroup>
+        <div className='size-main'>
+          <div className='size-boyut'>
+            <Label className='title-hamur'>Boyut Seç <span className='span-hamur'>*</span></Label>
+              <div className='size-boyut2'>
+                <Label>
+                  <Input type="radio" name="size" value="kucuk" onChange={handleChange} />Küçük</Label>
+                <Label>
+                  <Input type="radio" name="size" value="orta" onChange={handleChange} />Orta</Label>
+                <Label>
+                  <Input type="radio" name="size" value="buyuk" onChange={handleChange} />Büyük</Label>
+              </div>
+          </div>
+          <div className='size-hamur'>
+            <Label htmlFor="dough" className='title-hamur'>Hamur Seç <span className='span-hamur'>*</span></Label>
+            <Input type="select" name="dough" defaultValue={"-1"} value={formData.dough} onChange={handleChange}>
+              <option value="-1" disabled={true}>Hamur Kalınlığı Seçiniz</option>
+              {initalHamur.map((dough, ind) => (
+                <option key={ind} value={dough.name.toLowerCase()}>{dough.name}</option>
+              ))}
+            </Input>
+          </div>
         </div>
-        <FormGroup className='extra-css' check>
-                <div className='size-boyut3'>
-                    {extras.map((extra, ind) => (
-                    <div key={ind} className="extra-checkbox">
-                        <Label>
-                            <Input
-                                type="checkbox"
-                                onChange={handleChange}
-                                checked={formData.malzemeler.includes(extra.value)}
-                                name="malzemeler"
-                                value={extra.value}
-                                invalid={errors.warining}
-                            />
-                            {extra.label}
-                        </Label>
-                    </div>
-                    ))}
-                </div>
-         </FormGroup>
-         {errors.warining && <div data-cy="check-error" className='extra-label www'>{errorMessages.warining}</div>}
-        <div className='note'>
-        <FormGroup>
-        <Label for="note"><h2>Sipariş Notu</h2></Label>
-        <Input
-          type="textarea"
-          name="note"
-          id="note"
-          placeholder="Siparişinize eklemek istediğiniz bir not var mı?"
-          onChange={handleChange}
-          value={formData.note}
-          invalid={errors.note}
-        />
-        {errors.note && <FormFeedback data-cy="note-error">{errorMessages.note}</FormFeedback>}
       </FormGroup>
+        <div className='extra-label'>
+          <Label>
+            <h2>Ek Malzemeler</h2>
+            <p className='p-long'>Ek malzemelerinizi seçiniz. 5₺</p>
+          </Label>
+        </div>
+      <FormGroup className='extra-css' check>
+        <div className='size-boyut3'>
+          {extras.map((extra, ind) => (
+            <div key={ind} className="extra-checkbox">
+              <Label>
+                <Input
+                  type="checkbox"
+                  onChange={handleChange}
+                  checked={formData.malzemeler.includes(extra.value)}
+                  name="malzemeler"
+                  value={extra.value}
+                  invalid={errors.warining}
+                />
+                {extra.label}
+              </Label>
+              </div>
+          ))}
+        </div>
+      </FormGroup>
+      {errors.warining && <div data-cy="check-error" className='extra-label www'>{errorMessages.warining}</div>}
+      <div className='note'>
+        <FormGroup>
+          <Label for="note"><h2>Sipariş Notu</h2></Label>
+          <Input
+            type="textarea"
+            name="note"
+            id="note"
+            placeholder="Siparişinize eklemek istediğiniz bir not var mı?"
+            onChange={handleChange}
+            value={formData.note}
+            invalid={errors.note}
+          />
+          {errors.note && <FormFeedback data-cy="note-error">{errorMessages.note}</FormFeedback>}
+        </FormGroup>
       </div>
       <div className='bottom-side'>
-      <FormGroup className="quantity-selector">
-        <Button onClick={() => handleQuantityChange(-1)} className="decrement btnn">-</Button>
-        <span className="quantity">{formData.quantity}</span>
-        <Button onClick={() => handleQuantityChange(1)} className="increment btnn">+</Button>
-      </FormGroup>
-      <div className='bottom-most'>
-      <FormGroup>
-        <div className="order-summary">
-          <h3>Sipariş Toplamı</h3>
-          <div className="summary-item">
-            <span>Seçimler</span>
-            <span>{(formData.totalPrice - (formData.quantity * 85.50)).toFixed(2)}₺</span>
-          </div>
-          <div className="summary-item total">
-            <span>Toplam</span>
-            <span>{formData.totalPrice.toFixed(2)}₺</span>
-          </div>
-        </div>
-          <Button color='warning' disabled={!isValid} className="order-button btnn">
-            SİPARİŞ VER
-          </Button>
-        </FormGroup>        
-      </div>     
+        <FormGroup className="quantity-selector">
+          <Button onClick={() => handleQuantityChange(-1)} className="decrement btnn">-</Button>
+          <span className="quantity">{formData.quantity}</span>
+          <Button onClick={() => handleQuantityChange(1)} className="increment btnn">+</Button>
+        </FormGroup>
+        <div className='bottom-most'>
+          <FormGroup>
+            <div className="order-summary">
+              <h3>Sipariş Toplamı</h3>
+              <div className="summary-item">
+                <span>Seçimler</span>
+                <span>{(formData.totalPrice - (formData.quantity * 85.50)).toFixed(2)}₺</span>
+              </div>
+              <div className="summary-item total">
+                <span>Toplam</span>
+                <span>{formData.totalPrice.toFixed(2)}₺</span>
+              </div>
+            </div>
+            <Button color='warning' disabled={!isValid} className="order-button btnn">SİPARİŞ VER</Button>
+          </FormGroup>        
+        </div>     
       </div>
-         
     </Form>
     </>
   )
--- src/SuccessPage.jsx
+++ src/SuccessPage.jsx
@@ -2,7 +2,33 @@ import './style.css';
 import './reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function SuccessPage() {
+function SuccessPage({orderData, extras, initalHamur}) {
+
+  //malzemeleri mapler
+  const getLabels = (Extras) => {
+    return Extras
+      .map((value) => {
+        const extra = extras.find((extra) => extra.value === value);
+        return extra ? extra.label : value;
+      })
+      .join(", ");
+  };
+
+  //hamur tipini seçer
+  const getDough = (doughType) => {
+    const dough = initalHamur.find((dough) => dough.name.toLowerCase() === doughType.toLowerCase());
+    return dough ? dough.name : doughType;
+  };
+
+  //hamur boyutunun baş harfini büyütür ve kucuk se ü leri ve ç yi değiştirir
+  const sizeHarf = (size) => {
+    let buyukSize = size.charAt(0).toUpperCase() + size.slice(1);
+
+    if (size === "kucuk") {
+      buyukSize = buyukSize.replace(/u/g, "ü").replace(/c/g, "ç");
+    }
+    return buyukSize;
+  }
 
   return (
       <>
@@ -13,6 +39,22 @@ function SuccessPage() {
             <div className='success-page-h1'>
                 <p>lezzetin yolda</p>
                 <h1>SİPARİŞ ALINDI</h1>
+            </div>
+            <div>
+              {orderData ? (
+                <div className="order-details">
+                  <p><strong>ID:</strong> {orderData.id}</p>
+                  <p><strong>Sipariş Zamanı:</strong> {new Date(orderData.createdAt).toLocaleString()}</p>
+                  <p><strong>Boyut:</strong> {sizeHarf(orderData.size)}</p>
+                  <p><strong>Hamur Tipi:</strong> {getDough(orderData.dough)}</p>
+                  <p><strong>Adet:</strong> {orderData.quantity}</p>
+                  <p><strong>Ek Malzemeler:</strong> {getLabels(orderData.malzemeler)}</p>
+                  <p><strong>Not:</strong> {orderData.note}</p>
+                  <p><strong>Ek Malzeme Fiyat:</strong> {orderData.extraCost}₺</p>
+                  <p><strong>Toplam Fiyat:</strong> {orderData.totalPrice}₺</p>
+                </div>
+              ) : (<p>Sipariş Yükleniyor, Lütfen Bekleyiniz...</p>)
+              }
             </div>
         </div>
       </>
