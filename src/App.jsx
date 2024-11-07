cf5867d17f4fbd68cf793fc0023adc66eee8027c
furkanozanyuce <furkanozanyuce@gmail.com>
2024-11-07 22:29:56 +0300

    removed the values of extras and dough type from mainform and added them to app to better control prop lifting, added them as props

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,73 @@ import './reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { useState } from 'react';
-import { Switch, Route, useLocation } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import MainForm from './MainForm';
 import MainPage from './MainPage';
 import SuccessPage from './SuccessPage';
 
 
+//malzemeler
+const extras = [{
+  value: "pepperoni",
+  label: "Pepperoni"
+}, {
+  value: "sosis",
+  label: "Sosis"
+}, {
+  value: "kanadaJambonu",
+  label: "Kanada Jambonu"
+}, {
+  value: "tavukIzgara",
+  label: "Tavuk Izgara"
+}, {
+  value: "sogan",
+  label: "Soğan"
+}, {
+  value: "domates",
+  label: "Domates"
+}, {
+  value: "misir",
+  label: "Mısır"
+}, {
+  value: "sucuk",
+  label: "Sucuk"
+}, {
+  value: "jalepeno",
+  label: "Jalepeno"
+}, {
+  value: "sarimsak",
+  label: "Sarımsak"
+}, {
+  value: "biber",
+  label: "Biber"
+}, {
+  value: "zeytin",
+  label: "Zeytin"
+}, {
+  value: "ananas",
+  label: "Ananas"
+}, {
+  value: "kabak",
+  label: "Kabak"
+}, {
+  value: "peynir",
+  label: "Peynir"
+}
+];
+
+//hamur
+const initalHamur = [
+  {name: "İnce"},
+  {name: "Normal"},
+  {name: "Kalın"}
+]
+
 
 function App() {
+
+  const [orderData, setOrderData] = useState(null);
 
   return (
       <>
@@ -20,10 +78,10 @@ function App() {
             <MainPage />
           </Route>
           <Route path="/orderpage">
-            <MainForm />
+            <MainForm setOrderData={setOrderData} extras={extras} initalHamur={initalHamur} />
           </Route>
           <Route path="/successpage">
-            <SuccessPage />
+            <SuccessPage orderData={orderData} extras={extras} initalHamur={initalHamur} />
           </Route>
         </Switch>
       </>
